b70f5d892e249be43d018950f67b394b5f362a50
Kosyachenko Daria <113111929+kosyachenkodaria@users.noreply.github.com>
2022-10-09 17:37:31 +0300

    Delete task4.py

--- task4.py
+++
@@ -1,9 +0,0 @@
-k = int(input("введите натуральное число: "))
-
-for i in range(1,k+1):
-    for j in range(1,i+1):
-        print(j,end='') 
-    
-    for m in range(i-1,0,-1):
-        print(m,end='')
-    print()
