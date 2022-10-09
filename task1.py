a89e0f2cb0737aafaf3c72b2e6da26a2084639e5
Kosyachenko Daria <113111929+kosyachenkodaria@users.noreply.github.com>
2022-10-09 17:38:02 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-def sum_list():
-    list = []
-    for i in range(10):
-        numbers = float(input("введите число: "))
-        list.append(numbers)
-    return sum(list)
-print(f"сумма чисел = {sum_list()}")
