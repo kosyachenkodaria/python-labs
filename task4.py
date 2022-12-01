f7f4f6c0027f39fb8365ec309376cd1914ae9fee
Kosyachenko Daria <113111929+kosyachenkodaria@users.noreply.github.com>
2022-12-01 19:24:18 +0300

    Delete task4.py

--- task4.py
+++
@@ -1,6 +0,0 @@
-dic = {1: 'апельсинка', 2: 'стул', 3: 'герантофобия.', 4: 'кирпич'}
-key = int(input('Введите номер от 1 до 4 '))
-def func(dictionary):
-    v = dictionary.get(key)
-    print(v)
-func(dic)
